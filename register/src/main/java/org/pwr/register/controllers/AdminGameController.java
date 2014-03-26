cbeeaf6b5f8f2d4dd9715307f1fe7229e2ac4669
booob <19mateusz92@gmail.com>
2014-03-26 23:30:51 +0100

    Dodano podzial uprawnien w UserGame, problemy z niezapisujacym sie spring-security.xml

--- register/src/main/java/org/pwr/register/controllers/AdminGameController.java
+++ register/src/main/java/org/pwr/register/controllers/AdminGameController.java
@@ -1,23 +1,37 @@
 package org.pwr.register.controllers;
 
+import java.util.List;
+
 import org.pwr.register.mapper.GameUserMapper;
+import org.pwr.register.model.UserGame;
 import org.pwr.register.service.UserGameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/commonGame/")
-public class UserGameController {
+@RequestMapping("/adminGame/")
+public class AdminGameController {
 
-	@Autowired
 	private GameUserMapper gameUserMapper;
 	
 	@Autowired
 	private UserGameService userGameService;
+
+	@RequestMapping(value = "/userGame/", method = RequestMethod.POST)
+	public boolean createGameForUser(@RequestBody UserGame userGame) {
+		return userGameService.addUserGame(userGame);
+	}
+
+	@RequestMapping("/allGames/")
+	public List getAllUsersGames() {
+		return gameUserMapper.mapList(userGameService.getAllUsersGames()); 
+	}
 
 	@RequestMapping(value = "/userGame/", method = RequestMethod.GET)
 	public Integer getUserGameByLogin() {
@@ -26,6 +40,11 @@ public class UserGameController {
 		String name = auth.getName();
 		
 		return userGameService.getUserGameByLogin(name);
+	}
+
+	@RequestMapping(value = "/newGame/{id}", method = RequestMethod.GET)
+	public boolean createNewGame(@PathVariable Integer id) {
+		return userGameService.createNewGame(id);
 	}
 
 }
